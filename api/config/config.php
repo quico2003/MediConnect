a3277eae6c35f9464bf11963222c5bccede99549
Quico2003 <blesapenadesquico@gmail.com>
2025-03-13 17:17:57 +0100

    gitignore added

--- api/config/config.php
+++ api/config/config.php
@@ -15,7 +15,7 @@ define("IS_DEV", TRUE);
 define('LOG_FILE_PATH', "$document_root/logs/bookbank.txt"); // used in helpers utils.php 
 define('LOG_ERROR_FILE_PATH', "$document_root/logs/bookbank_errors.txt"); // used in helpers utils.php 
 define('PUBLIC_API_KEY', 'Another12345Code');
-define('API_URL', 'http://172.16.0.143' . $middleware);
+define('API_URL', 'http://testfb.vidavia.net' . $middleware);
 
 //Database Config
 define('DB_HOST', "127.0.0.1");
